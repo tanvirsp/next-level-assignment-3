5ad0980ab8ec7a71ef125f809acdf158bb56013f
tanvirsp <tanvirgraphic@gmail.com>
2026-06-14 22:30:45 +0600

    All query done

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 
 
 -- CREATE DATABASE
-create database football_Ticket_Booking_System;
+create database football_ticket_booking_system;
 
 
 
@@ -97,6 +97,45 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 
 
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+select match_id, fixture, Round(base_ticket_price) from matches
+where tournament_category = 'Champions League' and match_status = 'Available'
 
 
+-- Query 2: Search for all users whose full names start with 'Tanvir' or contain the phrase 'Haque' (case-insensitive).
+select user_id, full_name, email from users
+where full_name Ilike 'Tanvir%' or full_name Ilike '%Haque%';
+
+
+-- Query 3: Retrieve all booking records where the payment status is missing (NULL), replacing the empty result with 'Action Required'.
+select 
+    booking_id,
+    user_id,
+    match_id,
+    coalesce(payment_status, 'Action Required') AS systematic_status
+from Bookings
+where payment_status IS NULL;
+
+
+
+-- Query 4: Retrieve match booking details along with the User's full name and the scheduled Match fixture teams.
+select booking_id, full_name, fixture, total_cost from bookings
+inner join users using(user_id)
+inner join matches using(match_id)
+
+
+-- Query 5: Display a comprehensive list of all users and their booking IDs, ensuring that fans who have never bought a ticket are still listed.
+select user_id, full_name, booking_id from users 
+full join bookings using(user_id)
+
+
+-- Query 6: Find all ticket bookings where the total cost is strictly higher than the average cost of all ticket bookings.
+select * from bookings
+where total_cost > (select avg(total_cost) from bookings )
+
+
+
+-- Query 7: Retrieve the top 2 most expensive matches sorted by base ticket price, skipping the absolute highest premium match.
+select match_id, fixture, base_ticket_price from matches
+order by base_ticket_price desc offset 1 limit 2
 
